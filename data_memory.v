8503c2afe975dea05c8388d3184c8cfec0cc9b5f
Vũ Hữu <huu.vm224002@sis.hust.edu.vn>
2025-08-28 23:03:28 +0700

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -1,22 +1,21 @@
 module data_memory (
-    input clk,
-    input memw,
-    input [31:0] address,
+    input        clk,
+    input        memw,             
+    input [31:0] address,          
     input [31:0] data_write,
-    output [31:0] data_read
+    output[31:0] data_read
 );
-    integer i;  
-    reg [31:0] ram [255:0];  
-    wire [7:0] ram_addr = address[8 : 1];  
+    reg [31:0] ram [0:255];         
+    wire [7:0] addr = address[9:2]; // bỏ 2 bit thấp để lấy word index
 
-    initial begin  
-        for(i=0;i<256;i=i+1)  
-            ram[i] <= 32'd0;  
+    integer i;
+    initial begin
+        for(i=0; i<256; i=i+1)
+            ram[i] = 32'd0;
     end
-    
-    always @(posedge clk) begin  
-        if (memw)  
-            ram[ram_addr] <= data_write;  
-    end  
-    assign data_read = ram[ram_addr];
-endmodule 
+
+    always @(posedge clk) begin
+        if (memw) ram[addr] <= data_write;
+    end
+    assign data_read = ram[addr];
+endmodule
