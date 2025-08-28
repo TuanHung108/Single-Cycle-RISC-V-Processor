e8c08c33ba54455856ecae2046d742c0e42f8812
Vũ Hữu <huu.vm224002@sis.hust.edu.vn>
2025-08-28 22:20:11 +0700

    Update imem.v

--- imem.v
+++ imem.v
@@ -1,22 +1,17 @@
 module imem #(
-    parameter COL = 32,          // số bit mỗi ô nhớ (1 ins = 32 bit)
-    parameter ROW = 256          // số dòng (số ins tối đa)
+    parameter COL = 32,          
+    parameter ROW = 256          
 )(
-    input  [31:0] pc,            // Program Counter
-    input iready,
-    output [31:0] ins    // Instruction output
+    input  [31:0] pc,   // read address   
+    output [31:0] ins   // intruction
 );
 
     // Bộ nhớ ins
     reg [COL-1:0] memory [0:ROW-1];
-    // Địa chỉ word-aligned: bỏ 2 bit thấp vì mỗi ins = 4 byte
-    wire [$clog2(ROW)-1:0] rom_addr = pc[31:2];
-    // Load chương trình từ file (hex hoặc bin)
-    initial begin
-        // Ví dụ dùng file .hex (mỗi dòng 1 ins 32-bit)
-        $readmemh("imem_data.txt", memory);
-    end
-    // Đọc combinational
-    assign ins = (iready == 1) ? memory[rom_addr] : 32'b0;
 
+    initial begin
+        $readmemb("imem_data.bin", memory);
+    end
+
+    assign ins = memory[pc[31:2]];
 endmodule
